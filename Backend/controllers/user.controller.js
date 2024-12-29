36ac10a1cca56805917a56765a7d881054038908
Karan <karansalviwork@gmail.com>
2024-12-30 01:11:04 +0530

    cookie setting

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -58,11 +58,20 @@ const loginUser = catchAsyncErrors(async (req, res) => {
     });
   }
 
-  return res.status(200).cookie(process.env.TOKEN_NAME, token).json({
-    success: true,
-    message: "User is successfully logged in.",
-    data: user,
-  });
+  return res
+    .status(200)
+    .cookie(process.env.TOKEN_NAME, token, {
+      domain: ".vercel.app",
+      path: "/",
+      sameSite: "None",
+      secure: true,
+      httpOnly: true,
+    })
+    .json({
+      success: true,
+      message: "User is successfully logged in.",
+      data: user,
+    });
 });
 
 // Logout user in our web app
